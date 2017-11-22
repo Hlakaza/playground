ec9e8f17e635cfaabde18ba0c7c57bccd35be54c
rcb-ferreira <ferreia.rcb@gmail.com>
2017-11-22 14:53:24 +0200

    Update latest changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { ButtonsComponent } from './buttons/buttons.component';
