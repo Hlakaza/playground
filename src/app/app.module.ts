f2373ec4bfd614d2ebd6da4109d835cb29cb47a5
rcb-ferreira <ferreia.rcb@gmail.com>
2017-11-22 14:48:22 +0200

    fix base path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { ButtonsComponent } from './buttons/buttons.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue: '/playground'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
