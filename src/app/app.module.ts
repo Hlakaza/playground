208abec7583016aa6b98049dc9a116c75043649b
Lucky Hlakaza <nhlalucky9@gmail.com>
2017-12-06 11:34:32 +0200

    changed primary color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { APP_BASE_HREF } from '@angular/common';
-
 import { AppComponent } from './app.component';
 import { ButtonsComponent } from './buttons/buttons.component';
 import { TypographyComponent } from './typography/typography.component';
 import { FormsComponent } from './forms/forms.component';
 import { GridComponent } from './grid/grid.component';
+import { ModalsComponent } from './modals/modals.component';
 
 
 
@@ -16,10 +16,11 @@ import { GridComponent } from './grid/grid.component';
     ButtonsComponent,
     TypographyComponent,
     FormsComponent,
-    GridComponent
+    GridComponent,
+    ModalsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
