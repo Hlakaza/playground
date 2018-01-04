c15ef30091f89bd86e71a386dc257b046321fd17
Lucky Hlakaza <nhlalucky9@gmail.com>
2018-01-04 08:34:31 +0200

    refactor(styleguide)game tiles

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-games',
@@ -6,10 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./games.component.scss']
 })
 export class GamesComponent implements OnInit {
-
+  @Input() gameInfo: any;
+  @Input() skeleton: any;
+  @Output() register = new EventEmitter<any>();
+  @Output() gameLaunch = new EventEmitter<any>();
+  hover: boolean = false;
+  tileImage: string;
+  isNew: string;
+  heat: number;
+  gameId: number;
+  gameName: string;
+  isMobile: boolean;
+  LPId: any;
+  GP: any;
+  BackgroundImage: any;
   constructor() { }
 
+  mouseEnter() {
+    this.hover = true;
+  }
+  mouseExit() {
+    this.hover = false;
+  }
+
   ngOnInit() {
+    if (this.gameInfo !== undefined) {
+      this.tileImage = "https://cdn1.dmgamingsystems.com" + this.gameInfo.tileImage;
+      this.isNew = this.gameInfo.isNewGN;
+      this.heat = this.gameInfo.heat;
+      this.gameId = this.gameInfo.gameId;
+      this.gameName = this.gameInfo.GN;
+      this.isMobile = this.gameInfo.isMobile;
+      this.LPId = this.gameInfo.LPId;
+      this.BackgroundImage = "https://cdn2.dmgamingsystems.com" + this.gameInfo.BackgroundImage;
+      if (this.gameInfo["GameInfoCode"] !== undefined) {
+        this.GP = this.gameInfo["GameInfoCode"]["GameProvider"];
+      }
+    }
   }
 
 }
